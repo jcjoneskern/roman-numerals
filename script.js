65562b5e3e32979f79fef23b35d0ee9723e335e7
J-C <jcjoneskern@gmail.com>
2017-01-05 13:51:43 -0500

    function outline

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ module.exports = convert;
 function convert(input) {
   var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  var numeral = "";
 
+  //feature 1: Arabic to Roman
+  if(isNaN(input)===false) {
+    var output;
+    for(var i = 0; i < arabic.length; i++) {
+
+
+    }
+    output = numeral;
+  }
 
   return output;
 }
